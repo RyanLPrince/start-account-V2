619f4074bd7a3c7d237c5180baac3df22c0d0d42
Ryan <ryanlprince@aol.com>
2018-04-23 12:11:18 +0100

    Added annotations to fields to change java object to entity

---
+++ src/main/java/com/qa/domain/Account.java
@@ -0,0 +1,46 @@
+package com.qa.domain;
+
+
+import javax.persistence.*;
+
+@Entity 
+public class Account {
+	
+	@Column(length=40)
+	private String firstName;
+	@Column(length=40)
+	private String secondName;
+	@Id @GeneratedValue(strategy=GenerationType.AUTO) @Column(length=30)
+	private String accountNumber;
+
+	public Account(String firstName, String secondName, String accountNumber) {
+		this.firstName = firstName;
+		this.secondName = secondName;
+		this.accountNumber = accountNumber;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getSecondName() {
+		return secondName;
+	}
+
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+}
